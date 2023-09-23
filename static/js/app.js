ac3e03d5c39a76e357c5aa53f028146192ba3ca4
Gozea <zea@archpad.localdomain>
2023-09-23 14:43:50 +0200

    had to fix css

---
+++ static/js/app.js
@@ -0,0 +1,34 @@
+// Animation on scroll
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+            entry.target.classList.remove("hidden");
+        }
+    });  
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Decoration button
+const iconList = ["fa-heart", "fa-ghost", "fa-gamepad", "fa-diamond", "fa-dragon", "fa-hand-fist", "fa-mug-hot", "fa-music", "fa-shield-halved", "fa-gears", "fa-code", "fa-bug", "fa-keyboard", "fa-terminal", "fa-palette", "fa-crosshair", "fa-stamp", "fa-splotch", "fa-paintbrush", "fa-hippo", "fa-bolt", "fa-gift", "fa-umbrella", "fa-fire", "fa-thumbs-up"]
+const animList = ["fa-beat", "fa-beat-fade", "fa-bounce", "fa-fade", "fa-flip", "fa-shake", "fa-spin", "fa-spin-reverse"]
+
+function get_random (list) {
+  return list[Math.floor((Math.random()*list.length))];
+}
+
+function addDecoration() {
+    const banner = document.querySelector("#banner");
+    const e = document.createElement("div");
+    e.classList.add("fa-solid")
+    e.classList.add(get_random(iconList));
+    e.classList.add(get_random(animList));
+    banner.appendChild(e);
+    e.style.position = "absolute"
+    e.style.left = Math.floor(Math.random()*100) + "%"
+    e.style.top = Math.floor(Math.random()*100) + "%"
+}
+
